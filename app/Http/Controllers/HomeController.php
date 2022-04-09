ef4a6569fa0e5ca34c668a2b4eee6943ee88a1c4
KELMAS35 <karuso35@gmail.com>
2022-04-09 18:47:22 +0300

    second commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        echo "Index Function";
+    }
+    public function test()
+    {
+        return view('home.test');
+    }
+    public function test2()
+    {
+        return view('home.test2');
+    }
+    
+    public function param($id,$number)
+    {
+        //echo "Parameter 1:" , $id;
+        //echo "<br>Parameter 2:" , $number;
+        //echo "<br>Sum Parameters:", $id+$number;
+      return view('home.test2',
+      [
+            'id' => $id,
+            'number' =>$number
+        ]);
+    }
+    public function save(Request $request)
+    {
+      //  echo "Save Function<br>";
+      //  echo "Name :",$_REQUEST["fname"];
+      //  echo "Last Name :",$_REQUEST["lname"];
+        return view('home.test',
+      [
+            'fname' =>$_REQUEST["fname"],
+            'lname' =>$_REQUEST["lname"]
+        ]);
+    }
+}
---
+++ resources/views/home/test2.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>PARAMETRE YERİ</h1>
+<p>This is a paragraph.</p>
+Id Value : {{$id}}
+<br>
+Number Value : {{$number}}
+<br>
+Sum params : {{$id + $number}}
+
+</form>
+
+</body>
+</html>
